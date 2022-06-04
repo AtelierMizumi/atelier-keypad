5db11c54aeba73fbad21b290ef04bee55c4b6819
AtelierMizumi <thuanc177@gmail.com>
2022-06-05 05:51:56 +0700

    update pcb + qmk firmware

--- qmk_firmware/keyboards/kb/config.h
+++ qmk_firmware/keyboards/kb/config.h
@@ -8,8 +8,8 @@
 #define PRODUCT_ID      0x6060
 #define DEVICE_VER      0x0001
 #define MANUFACTURER    qmkbuilder
-#define PRODUCT         keyboard
-#define DESCRIPTION     Keyboard
+#define PRODUCT         Alter6k
+#define DESCRIPTION     The Alter6k keypad
 
 /* key matrix size */
 #define MATRIX_ROWS 3
@@ -21,12 +21,12 @@
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
-#define DIODE_DIRECTION ROW2COL
+#define DIODE_DIRECTION COL2ROW
 
 /* number of backlight levels */
 
-#ifdef BACKLIGHT_PIN
-#define BACKLIGHT_LEVELS 6
+#ifdef BACKLIGHT_PIN D4
+#define BACKLIGHT_LEVELS 5
 #endif
 
 /* Set 0 if debouncing isn't needed */
@@ -46,13 +46,23 @@
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
+/*Extra stuffs I added myself */
+#define USB_POLLING_INTERVAL_MS 1
+#define FORCE_NKRO
+#define BOOTMAGIC_LITE_ROW 0
+#define BOOTMAGIC_LITE_COLUMN 0
 
-#ifdef RGB_DI_PIN
+#ifdef RGB_DI_PIN D0
 #define RGBLIGHT_ANIMATIONS
 #define RGBLED_NUM 4
 #define RGBLIGHT_HUE_STEP 8
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
+#define RGBLIGHT_EFFECT_BREATHING
+#define RGBLIGHT_EFFECT_RAINBOW_MOOD
+#define RGBLIGHT_EFFECT_RAINBOW_SWIRL
+#define RGBLIGHT_EFFECT_GRADIENT
+#define RGBLIGHT_DEFAULT_MODE RGBLIGHT_MODE_RAINBOW_MOOD
 #endif
 
 #endif
